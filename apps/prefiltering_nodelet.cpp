af13fff6aa0b827e8e409d9d27cf2209f404207a
Weijie Wang <duangsir518@163.com>
2025-02-25 19:54:51 +0100

    Convert back to pcl::PointCloud

--- apps/prefiltering_nodelet.cpp
+++ apps/prefiltering_nodelet.cpp
@@ -103,18 +103,11 @@ private:
     imu_queue.push_back(imu_msg);
   }
 
-  void cloud_callback(const sensor_msgs::PointCloud2ConstPtr& cloud_msg) {
-    pcl::PointCloud<PointT>::Ptr src_cloud_r(new pcl::PointCloud<PointT>());
-    
-    // Convert the PointCloud2 message to pcl::PointCloud
-    pcl::fromROSMsg(*cloud_msg, *src_cloud_r);
-
-    // Check if the cloud is empty
-    if (src_cloud_r->empty()) {
+  void cloud_callback(const pcl::PointCloud<PointT>& src_cloud_r) {
+    pcl::PointCloud<PointT>::ConstPtr src_cloud = src_cloud_r.makeShared();
+    if(src_cloud->empty()) {
       return;
     }
-
-    pcl::PointCloud<PointT>::ConstPtr src_cloud = src_cloud_r; // No need for makeShared()
 
     src_cloud = deskewing(src_cloud);
 
